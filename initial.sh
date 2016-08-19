6d4df614c7ac8ee22f6c643eea59494f17919534
Shizhan Zhu <zhusz@users.noreply.github.com>
2016-08-19 13:43:27 +0800

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -7,7 +7,7 @@ wget https://www.dropbox.com/s/kwmbfrl51jvy1ej/image_source.tar.gz
 tar -xzf image_source.tar.gz
 rm image_source.tar.gz
 cd ..
-wget https://www.dropbox.com/s/f5rfh96s2j3bk7v/Model.tar.gz
+wget https://www.dropbox.com/s/x67u4c35sn1cwv2/Model.tar.gz
 tar -xzf Model.tar.gz
 rm Model.tar.gz
 cd ..
